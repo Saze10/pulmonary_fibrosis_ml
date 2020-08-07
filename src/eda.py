aa689ac6965e48512e55e148176e7737a4308f9e
Saze <saze@pop-os.localdomain>
2020-08-07 20:48:11 +1000

    some eda and basic prelim stuff

--- src/eda.py
+++ src/eda.py
@@ -7,14 +7,25 @@ from IPython.display import Image as show_gif
 import scipy.misc
 import pydicom 
 import seaborn as sns
+from fastai2.basics import *
+from fastai2.callback.all import *
+from fastai2.vision.all import *
+from fastai2.medical.imaging import *
 
+#train_df = pd.read_csv('../data/train.csv')
+#test_df = pd.read_csv('../data/test.csv')
 
-train_df = pd.read_csv('../data/train.csv')
-test_df = pd.read_csv('../data/test.csv')
+#print('Shape of Training data: ', train_df.shape)
+#print('Shape of Test data: ', test_df.shape)
 
-print('Shape of Training data: ', train_df.shape)
-print('Shape of Test data: ', test_df.shape)
+#print(f"The total patient ids are {train_df['Patient'].count()}")
+#print(f"Number of unique ids are {train_df['Patient'].value_counts().shape[0]} ")
 
-print(f"The total patient ids are {train_df['Patient'].count()}")
-print(f"Number of unique ids are {train_df['Patient'].value_counts().shape[0]} ")
-
+source = Path('../data')
+train = source/'train'
+train_files = get_dicom_files(train)
+info_view = train_files[2133]
+dimg = dcmread(info_view)
+print(dimg.PixelData[:200])
+plt.imshow(dimg.pixel_array)
+plt.show()
--- src/metrics.py
+++ src/metrics.py
@@ -179,7 +179,7 @@ def laplace_log_likelihood(actual_fvc, predicted_fvc, confidence, return_values 
     """
     sd_clipped = np.maximum(confidence, 70)
     delta = np.minimum(np.abs(actual_fvc - predicted_fvc), 1000)
-    metric = - np.sqrt(2) * delta / sd_clipped - np.log(np.sqrt(2) * sd_clipped)
+    metric = - ((np.sqrt(2) * delta) / sd_clipped) - np.log(np.sqrt(2) * sd_clipped)
 
     if return_values:
         return metric
